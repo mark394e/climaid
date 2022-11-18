3c688fd199e329db3a524f48837c64d17d1618c3
Markus <mark394e@stud.kea.dk>
2022-11-18 11:33:50 +0100

    jeg skal arbejde i branch

---
+++ pages/components/SocialMediaProvider.js
@@ -0,0 +1,11 @@
+function SocialMediaProvider() {
+  return (
+    <>
+      <img src=""></img>
+      <h3>Youtube</h3>
+      <div className="slidecontainer"></div>
+    </>
+  );
+}
+
+export default SocialMediaProvider;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>hrello</div>;
+  return <div>hello</div>;
 }
