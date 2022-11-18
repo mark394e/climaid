9f65b7c323ed0d1c1d08fe9a2b60b3548711078b
Markus <mark394e@stud.kea.dk>
2022-11-18 13:16:17 +0100

    lidt styling baby

--- pages/components/SocialMediaProvider.js
+++ pages/components/SocialMediaProvider.js
@@ -4,7 +4,7 @@ function SocialMediaProvider(props) {
       <div className="">{props.logo}</div>
       <h3>{props.name}</h3>
       <div className="slidecontainer">
-        <input type="range" min="1" max="10" defaultValue="0"></input>
+        <input type="range" min="0" max="24" defaultValue="0" step="0.5"></input>
       </div>
     </article>
   );
