382bfc733f4d8b5c511c7364252345f1558eabf0
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2022-11-18 14:22:42 +0100

    getting value from input

--- pages/components/SocialMediaProvider.js
+++ pages/components/SocialMediaProvider.js
@@ -1,10 +1,40 @@
+import React, { useRef, useState } from "react";
+
 function SocialMediaProvider(props) {
+  const [hours, setHours] = useState(0);
+
+  const handleChange = (event) => {
+    props.setSocialMedia((prevState) =>
+      prevState.map((sm) => {
+        if (sm.name === props.name) {
+          const newSm = { ...sm };
+          newSm.minutes = Number(event.target.value);
+          return newSm;
+        }
+        return sm;
+      })
+    );
+    console.log("value is:", event.target.value);
+  };
+
+  // props.minutes = hours;
+  console.log(typeof +hours);
+
   return (
     <article className="some-article">
-      <div className="">{props.logo}</div>
-      <h3>{props.name}</h3>
+      <div className="article-top">
+        <div className="logo">{props.logo}</div>
+        <h3>{props.name}</h3>
+      </div>
       <div className="slidecontainer">
-        <input type="range" min="0" max="24" defaultValue="0" step="0.5"></input>
+        <input
+          onChange={handleChange}
+          type="range"
+          min="0"
+          max="24"
+          defaultValue="0"
+          step="0.5"
+        />
       </div>
     </article>
   );
