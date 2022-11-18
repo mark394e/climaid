af9b4bf8220438b6411671d970ef098907603f35
Markus <mark394e@stud.kea.dk>
2022-11-18 13:44:04 +0100

    styling

--- pages/components/SocialMediaProvider.js
+++ pages/components/SocialMediaProvider.js
@@ -1,8 +1,10 @@
 function SocialMediaProvider(props) {
   return (
     <article className="some-article">
-      <div className="">{props.logo}</div>
-      <h3>{props.name}</h3>
+      <div className="article-top">
+        <div className="logo">{props.logo}</div>
+        <h3>{props.name}</h3>
+      </div>
       <div className="slidecontainer">
         <input type="range" min="0" max="24" defaultValue="0" step="0.5"></input>
       </div>
