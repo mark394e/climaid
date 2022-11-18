ef8f9141f3d22673f31a4486a661845452a97703
Markus <mark394e@stud.kea.dk>
2022-11-18 12:50:24 +0100

    SoMe article med slider

--- pages/components/SocialMediaProvider.js
+++ pages/components/SocialMediaProvider.js
@@ -1,11 +1,12 @@
-import { Range } from "react-range";
-
 function SocialMediaProvider(props) {
   return (
-    <>
-      <div>{props.logo}</div>
+    <article className="some-article">
+      <div className="">{props.logo}</div>
       <h3>{props.name}</h3>
-    </>
+      <div className="slidecontainer">
+        <input type="range" min="1" max="10" defaultValue="0"></input>
+      </div>
+    </article>
   );
 }
 
