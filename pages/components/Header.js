4d23c3ec4a74f3be04f4dd681afe3b2cabdaa14d
Markus <mark394e@stud.kea.dk>
2022-11-21 14:18:07 +0100

    header styling

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -5,13 +5,12 @@ export default function Header() {
       <section>
         <p>
           You have heard it before, but it can not be told again:
-          <span className="bold"> The carbon-emissions are rising!</span> And
-          guess what? Social media is a huge sinner. This calculator reveals the
-          carbon emission behind your use of Social media.
+          <span className="bold"> The carbon-emissions are rising!</span> And guess what? Social
+          media is a huge sinner. This calculator reveals the carbon emission behind your use of
+          Social media.
         </p>
         <p>Ready to reveal your use?</p>
       </section>
-      <button>Lets get started!</button>
     </header>
   );
 }
