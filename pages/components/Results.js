e772e506efb536230cfc0982fb98549484f698e9
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2022-11-23 13:04:52 +0100

    result responsive semi-fixed

--- pages/components/Results.js
+++ pages/components/Results.js
@@ -20,143 +20,151 @@ function Results(props) {
 
   return (
     <>
-      <BubbleChart></BubbleChart>
+      {" "}
       <h2>RESULT</h2>
-      <div className="co2day">
-        <h3>
-          {co2.toFixed(2)} kg. CO<sub>2</sub> a day!
-        </h3>
-        <h4>
-          With your CO<sub>2</sub> footprint you can produce...
-        </h4>
-        <div>
-          <Produce
-            productName="beef"
-            amount={beef}
-            productName2="falafel"
-            amount2={falafel}
-          ></Produce>
+      <div className="result_illustrations">
+        <BubbleChart></BubbleChart>
+        <div className="co2day">
+          <h3>
+            {co2.toFixed(2)} kg. CO<sub>2</sub> a day!
+          </h3>
+          <h4>
+            With your CO<sub>2</sub> footprint you can produce...
+          </h4>
+          <div>
+            <Produce
+              productName="beef"
+              amount={beef}
+              productName2="falafel"
+              amount2={falafel}
+            ></Produce>
+          </div>
+          <div className="breakline"></div>
+          <div>
+            <ProduceL
+              productName="dairy milk"
+              amount={dairy}
+              productName2="oat milk"
+              amount2={oat}
+            ></ProduceL>
+          </div>
+          <div className="breakline"></div>
+          <div>
+            <Produce
+              productName="rice"
+              amount={rice}
+              productName2="potatoes"
+              amount2={potatoes}
+            ></Produce>
+          </div>
         </div>
-        <div className="breakline"></div>
-        <div>
-          <ProduceL
-            productName="dairy milk"
-            amount={dairy}
-            productName2="oat milk"
-            amount2={oat}
-          ></ProduceL>
+        {showMore && (
+          <>
+            <div className="co2week">
+              <h3>
+                {(co2 * 7).toFixed(2)} kg. CO<sub>2</sub> a week!
+              </h3>
+              <h4>Same as...</h4>
+              <div>
+                <Produce
+                  productName="beef"
+                  amount={beef * 7}
+                  productName2="falafel"
+                  amount2={falafel * 7}
+                ></Produce>
+              </div>
+              <div className="breakline"></div>
+              <div>
+                <ProduceL
+                  productName="dairy milk"
+                  amount={dairy * 7}
+                  productName2="oat milk"
+                  amount2={oat * 7}
+                ></ProduceL>
+              </div>
+              <div className="breakline"></div>
+              <div>
+                <Produce
+                  productName="rice"
+                  amount={rice * 7}
+                  productName2="potatoes"
+                  amount2={potatoes * 7}
+                ></Produce>
+              </div>
+            </div>
+            <div className="co2month">
+              <h3>
+                {(co2 * 30).toFixed(2)} kg. CO<sub>2</sub> a month!
+              </h3>
+              <h4>Same as...</h4>
+              <div>
+                <Produce
+                  productName="beef"
+                  amount={beef * 30}
+                  productName2="falafel"
+                  amount2={falafel * 30}
+                ></Produce>
+              </div>
+              <div className="breakline"></div>
+              <div>
+                <ProduceL
+                  productName="dairy milk"
+                  amount={dairy * 30}
+                  productName2="oat milk"
+                  amount2={oat * 30}
+                ></ProduceL>
+              </div>
+              <div className="breakline"></div>
+              <div>
+                <Produce
+                  productName="rice"
+                  amount={rice * 30}
+                  productName2="potatoes"
+                  amount2={potatoes * 30}
+                ></Produce>
+              </div>
+            </div>
+          </>
+        )}
+        <div className="co2year">
+          <h3>
+            {(365 * co2).toFixed(2)} kg. CO<sub>2</sub> a year!
+          </h3>
+          <h4>Same as...</h4>
+          <div>
+            <Produce
+              productName="beef"
+              amount={beef * 365}
+              productName2="falafel"
+              amount2={falafel * 365}
+            ></Produce>
+          </div>
+          <div className="breakline"></div>
+          <div>
+            <ProduceL
+              productName="dairy milk"
+              amount={dairy * 365}
+              productName2="oat milk"
+              amount2={oat * 365}
+            ></ProduceL>
+          </div>
+          <div className="breakline"></div>
+          <div>
+            <Produce
+              productName="rice"
+              amount={rice * 365}
+              productName2="potatoes"
+              amount2={potatoes * 365}
+            ></Produce>
+          </div>
         </div>
-        <div className="breakline"></div>
-        <div>
-          <Produce
-            productName="rice"
-            amount={rice}
-            productName2="potatoes"
-            amount2={potatoes}
-          ></Produce>
+        <div className="clickbutton">
+          <ShowMoreBtn
+            setShowMore={setShowMore}
+            showMore={showMore}
+          ></ShowMoreBtn>
         </div>
       </div>
-      {showMore && (
-        <>
-          <div className="co2week">
-            <h3>
-              {(co2 * 7).toFixed(2)} kg. CO<sub>2</sub> a week!
-            </h3>
-            <h4>Same as...</h4>
-            <div>
-              <Produce
-                productName="beef"
-                amount={beef * 7}
-                productName2="falafel"
-                amount2={falafel * 7}
-              ></Produce>
-            </div>
-            <div className="breakline"></div>
-            <div>
-              <ProduceL
-                productName="dairy milk"
-                amount={dairy * 7}
-                productName2="oat milk"
-                amount2={oat * 7}
-              ></ProduceL>
-            </div>
-            <div className="breakline"></div>
-            <div>
-              <Produce
-                productName="rice"
-                amount={rice * 7}
-                productName2="potatoes"
-                amount2={potatoes * 7}
-              ></Produce>
-            </div>
-          </div>
-          <div className="co2month">
-            <h3>
-              {(co2 * 30).toFixed(2)} kg. CO<sub>2</sub> a month!
-            </h3>
-            <h4>Same as...</h4>
-            <div>
-              <Produce
-                productName="beef"
-                amount={beef * 30}
-                productName2="falafel"
-                amount2={falafel * 30}
-              ></Produce>
-            </div>
-            <div className="breakline"></div>
-            <div>
-              <ProduceL
-                productName="dairy milk"
-                amount={dairy * 30}
-                productName2="oat milk"
-                amount2={oat * 30}
-              ></ProduceL>
-            </div>
-            <div className="breakline"></div>
-            <div>
-              <Produce
-                productName="rice"
-                amount={rice * 30}
-                productName2="potatoes"
-                amount2={potatoes * 30}
-              ></Produce>
-            </div>
-          </div>
-        </>
-      )}
-      <div className="co2year">
-        <h3>
-          {(365 * co2).toFixed(2)} kg. CO<sub>2</sub> a year!
-        </h3>
-        <h4>Same as...</h4>
-        <div>
-          <Produce
-            productName="beef"
-            amount={beef * 365}
-            productName2="falafel"
-            amount2={falafel * 365}
-          ></Produce>
-        </div>
-        <div className="breakline"></div>
-        <div>
-          <ProduceL
-            productName="dairy milk"
-            amount={dairy * 365}
-            productName2="oat milk"
-            amount2={oat * 365}
-          ></ProduceL>
-        </div>
-        <div className="breakline"></div>
-        <div>
-          <Produce
-            productName="rice"
-            amount={rice * 365}
-            productName2="potatoes"
-            amount2={potatoes * 365}
-          ></Produce>
-        </div>
-      </div>
-      <ShowMoreBtn setShowMore={setShowMore} showMore={showMore}></ShowMoreBtn>
     </>
   );
 }
@@ -166,7 +174,8 @@ export default Results;
 function Produce(props) {
   return (
     <p>
-      {props.amount} kg {props.productName} or {props.amount2} kg {props.productName2}.
+      {props.amount} kg {props.productName} or {props.amount2} kg{" "}
+      {props.productName2}.
     </p>
   );
 }
@@ -174,7 +183,8 @@ function Produce(props) {
 function ProduceL(props) {
   return (
     <p>
-      {props.amount} L {props.productName} or {props.amount2} L {props.productName2}
+      {props.amount} L {props.productName} or {props.amount2} L{" "}
+      {props.productName2}
     </p>
   );
 }
